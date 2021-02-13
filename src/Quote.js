d3b2603be280353ee57449a7aa2bf62f60dd8d55
Vedaant Dutt <vedaantdutt@gmail.com>
2021-02-13 11:45:10 +0900

    addition of backend login-generate random quote

--- src/Quote.js
+++ src/Quote.js
@@ -4,13 +4,23 @@ import quotes from "./data/quotes";
 
 function Quote() {
 
+  // Initialize the quote state with the first quote 
+  const [quote, setQuote] = useState(quotes[0]);
+
+  // Function to generate a new quote
+  const generateQuoteButton = () => {
+    const lengthQuotes=quotes.length;
+    const randomIndex = Math.floor(Math.random() * lengthQuotes);
+    setQuote(quotes[randomIndex]);
+  };
+
   // Return the quote and author
   return (
     <div className="quote-box">
-      <p className="quote-text"></p>
+      <p className="quote-text">"{quote.quote}"</p>
      
-      <p className="quote-author"></p>
-      <button>Generate Quote</button>
+      <p className="quote-author">- by {quote.author}</p>
+      <button onClick={generateQuoteButton}>Generate Quote</button>
     </div>
   );
 }
